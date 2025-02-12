4f9283314270ba8ab734e681d58175d7ecbb2494
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-02-12 12:44:39 +0600

    deployment

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class CheckAdmin
@@ -14,15 +15,18 @@ class CheckAdmin
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-         if (!$request->user() || !$this->isAdmin($request->user())) {
+
+        Log::info("request" . $request->user());
+        if (!$request->user() || !$this->isAdmin($request->user())) {
             return response()->json(['message' => 'Authentication failed'], 403);
         }
         return $next($request);
     }
 
-    public function isAdmin($user){
-        return $user->role =='admin' ? true : false;
+    public function isAdmin($user)
+    {
+        return $user->role == 'admin' ? true : false;
     }
 }
