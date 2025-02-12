8368445f69143ae796812721a39512c2a9c6c5bc
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-02-12 16:22:54 +0600

    trip

--- app/Repository/Services/Trip/TripService.php
+++ app/Repository/Services/Trip/TripService.php
@@ -8,7 +8,8 @@ use Exception;
 use Illuminate\Support\Facades\Log;
 use DB;
 
-class TripService implements CommonInterface{
+class TripService implements CommonInterface
+{
 
     protected $contact;
 
@@ -17,66 +18,67 @@ class TripService implements CommonInterface{
      *
      * @return bool
      */
-    public function index($page,$search)
+    public function index($page, $search)
     {
 
-        try{
+        try {
             $perPage = 10;
             $availableSeats = DB::table('trips')
-            ->join('vehicles','trips.vehicle_id','=','vehicles.id')
-            ->join('routes','trips.route_id','=','routes.id')
-            ->where('is_active',1)
-            ->orWhere('trip_name', 'like', '%' . $search . '%')
-            ->orWhere('price', 'like', '%' . $search . '%')
-            ->paginate($perPage, ['trips.id','trip_name','departure_time','arrival_time','price','vehicle_name','route_name'], 'page', $page);
-        
-           return $availableSeats;
-        }catch(Exception $ex){
+                ->join('vehicles', 'trips.vehicle_id', '=', 'vehicles.id')
+                ->join('routes', 'trips.route_id', '=', 'routes.id')
+                ->where('is_active', 1)
+                ->orWhere('trip_name', 'like', '%' . $search . '%')
+                ->orWhere('price', 'like', '%' . $search . '%')
+                ->paginate($perPage, ['trips.id', 'trip_name', 'departure_time', 'arrival_time', 'price', 'vehicle_name', 'route_name'], 'page', $page);
+
+            return $availableSeats;
+        } catch (Exception $ex) {
             Log::alert($ex->getMessage());
         }
     }
 
-    public function store($request){
-        try{
+    public function store($request)
+    {
+        try {
             $routeInsert = DB::table('trips')->insert($request);
-            Log::info("Trip inserted: ".$routeInsert);
-            if($routeInsert){
+            Log::info("Trip inserted: " . $routeInsert);
+            if ($routeInsert) {
                 return true;
             }
             return false;
-        }catch(Exception $ex){
-            Log::alert("Insert error: ".$ex->getMessage());
+        } catch (Exception $ex) {
+            Log::alert("Insert error: " . $ex->getMessage());
         }
     }
 
 
-    public function findById($id){
-        try{
-            $route = DB::table('trips')->where('id',$id)->first();
+    public function findById($id)
+    {
+        try {
+            $route = DB::table('trips')->where('id', $id)->first();
             return $route;
-        }catch(Exception $ex){
-            Log::alert("Find By Id Error".$ex->getMessage());
+        } catch (Exception $ex) {
+            Log::alert("Find By Id Error" . $ex->getMessage());
         }
     }
 
-    public function delete($id){
-        try{
-            $response =DB::table('seats')->where('id',$id)->delete();
+    public function delete($id)
+    {
+        try {
+            $response = DB::table('seats')->where('id', $id)->delete();
             return $response;
-        }catch(Exception $ex){
-            Log::alert("Delete Error".$ex->getMessage());
+        } catch (Exception $ex) {
+            Log::alert("Delete Error" . $ex->getMessage());
         }
     }
 
-    public function findAllActiveTrips($data){
-        try{
+    public function findAllActiveTrips($data)
+    {
+        try {
             $query = DB::table('trips')
-                    ->join('seat_availablities', 'trips.id', '=', 'seat_availablities.trip_id')
-                    ->join('vehicles', 'trips.vehicle_id', '=', 'vehicles.id')
-                    ->join('routes', 'trips.route_id', '=', 'routes.id')
-                    ->select('trips.trip_name','trips.departure_time','trips.arrival_time','trips.price','vehicles.vehicle_name','vehicles.vehicle_type','routes.route_name', DB::raw('COUNT(seat_availablities.id) as available_seats'))
-                    ->groupBy('trips.id'); 
-            
+                ->select('trips.trip_name', 'trips.departure_time', 'trips.arrival_time', 'trips.price')
+                ->groupBy('trips.id');
+
             if (isset($data['trip_name']) && $data['trip_name']) {
                 $query->where('trip_name', 'like', '%' . $data['trip_name'] . '%');
             }
@@ -89,12 +91,11 @@ class TripService implements CommonInterface{
 
             $trips = $query->get();
 
-            
+
 
             return $trips;
-        }catch(Exception $ex){
+        } catch (Exception $ex) {
             Log::alert($ex->getMessage());
         }
     }
-
 }
