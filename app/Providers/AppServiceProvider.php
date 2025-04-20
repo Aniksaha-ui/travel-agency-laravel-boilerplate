4d883f8feb0b28c6565579264556649699e75898
Aniksaha-ui <sahaanik1045@gmail.com>
2025-04-20 13:46:50 -0400

    travel booking migration file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Repository\Interfaces\RouteInterface;
 use App\Repository\Services\RouteService;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
