cb0b50a3edaff31be55cc4aee44fbb4a8a004d47
Anik <sahaanik1045@gmail.com>
2025-04-20 23:04:28 +0600

    merge

--- database/migrations/2024_10_07_101714_create_trips_table.php
+++ database/migrations/2024_10_07_101714_create_trips_table.php
@@ -15,16 +15,23 @@ class CreateTripsTable extends Migration
     {
         Schema::create('trips', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('vehicle_id'); 
-            $table->unsignedBigInteger('route_id'); 
+            $table->unsignedBigInteger('vehicle_id');
+            $table->unsignedBigInteger('route_id');
             $table->string('trip_name');
+            $table->longText('description')->charset('utf8mb4')->collation('utf8mb4_general_ci');
             $table->dateTime('departure_time');
+            $table->string('image')->charset('utf8mb4')->collation('utf8mb4_general_ci');
             $table->dateTime('arrival_time');
-            $table->decimal('price', 8, 2); 
+            $table->decimal('price', 8, 2);
             $table->boolean('is_active')->default(1);
             $table->timestamps();
+            $table->string('status')->charset('utf8mb4')->collation('utf8mb4_general_ci');
+
             $table->foreign('vehicle_id')->references('id')->on('vehicles')->onDelete('cascade');
             $table->foreign('route_id')->references('id')->on('routes')->onDelete('cascade');
+
+            $table->index('vehicle_id');
+            $table->index('route_id');
         });
     }
 
