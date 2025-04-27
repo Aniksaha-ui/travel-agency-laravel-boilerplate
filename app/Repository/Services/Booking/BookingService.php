1770d6ff93171a925b283f6855877f2dea859944
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-04-27 14:44:18 +0600

    booking information added

--- app/Repository/Services/Booking/BookingService.php
+++ app/Repository/Services/Booking/BookingService.php
@@ -23,7 +23,7 @@ class BookingService
                 ->where('bookings.seat_ids', 'like', '%' . $search . '%')
                 ->orWhere('trips.trip_name', 'like', '%' . $search . '%')
                 ->orWhere('trips.price', 'like', '%' . $search . '%')
-                ->paginate($perPage, ['bookings.id', 'bookings.user_id', 'bookings.seat_ids', 'bookings.booking_type', 'bookings.created_at', 'bookings.status', 'bookings.booking_type', 'trips.trip_name', 'trips.price', 'users.name', 'packages.name','users.name','users.email'], 'page', $page);
+                ->paginate($perPage, ['bookings.id', 'bookings.user_id', 'bookings.seat_ids', 'bookings.booking_type', 'bookings.created_at', 'bookings.status', 'bookings.booking_type', 'trips.trip_name', 'trips.price', 'users.name as username', 'packages.name','users.name','users.email'], 'page', $page);
             return $bookings;
         } catch (Exception $ex) {
             Log::alert($ex->getMessage());
