eee8630ec0652b3eebd81f690271c36bdd9b4a30
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-05-06 12:40:03 +0600

    booking a vehicle modify

--- app/Http/Controllers/Admin/Vehicles/VehiclesController.php
+++ app/Http/Controllers/Admin/Vehicles/VehiclesController.php
@@ -17,96 +17,104 @@ class VehiclesController extends Controller
         $this->vehicleService = $vehicleService;
     }
 
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $page = $request->query('page');
         $search = $request->query('search');
-      
-        $response = $this->vehicleService->index($page,$search);
+
+        $response = $this->vehicleService->index($page, $search);
         return response()->json([
-            "data"=> $response,
-            "message"=> "success"
-        ],200);
+            "data" => $response,
+            "message" => "success"
+        ], 200);
     }
 
-    public function insert(Request $request){
-        try{
-           $response = $this->vehicleService->store($request->all());
-           if($response==true){
-            return response()->json([
-                'isExecute' => true,
-                'data' => $response,
-                'message' => 'New Vehicle Created',
-            ],200);
-           }
-        }catch(Exception $ex){
-           Log::error($ex->getMessage());
+    public function insert(Request $request)
+    {
+        try {
+            $response = $this->vehicleService->store($request->all());
+            if ($response == true) {
+                return response()->json([
+                    'isExecute' => true,
+                    'data' => $response,
+                    'message' => 'New Vehicle Created',
+                ], 200);
+            }
+        } catch (Exception $ex) {
+            Log::error($ex->getMessage());
         }
-       }
-   
-       public function findVehicleById($id){
-           try{
-               $response = $this->vehicleService->findById($id);
-               if($response){
-                   return response()->json([
-                       "isExecute"=> true,
-                       "data"=> $response,
-                       "message"=> "Find Single Vehicle"
-                   ],200);
-               }else{
-                   return response()->json([
-                       "isExecute"=> true,
-                       "message"=> "No Data Found"
-                   ],200);
-               }
-   
-           }catch(Exception $ex){
-               Log::error($ex->getMessage());
-           }
-       }
-   
-   
-       public function delete($id){
-           try{
-               $response = $this->vehicleService->delete($id);
-               if($response){
-                   return response()->json([
-                       "isExecute"=> true,
-                       "data"=> $response,
-                       "message"=> "Vehicle Deleted"
-                   ],200);
-               }else{
-                   return response()->json([
-                       "isExecute"=> true,
-                       "message"=> "Data Can not be deleted"
-                   ],200);
-               }
-           }catch(Exception $ex){
-               Log::error($ex->getMessage());
-           }
-       }
+    }
 
-       public function dropdown(){
-            $response = $this->vehicleService->dropdown();
-            return response()->json([
-                "data"=> $response,
-                "message"=> "success"
-            ],200);
-       }
+    public function findVehicleById($id)
+    {
+        try {
+            $response = $this->vehicleService->findById($id);
+            if ($response) {
+                return response()->json([
+                    "isExecute" => true,
+                    "data" => $response,
+                    "message" => "Find Single Vehicle"
+                ], 200);
+            } else {
+                return response()->json([
+                    "isExecute" => true,
+                    "message" => "No Data Found"
+                ], 200);
+            }
+        } catch (Exception $ex) {
+            Log::error($ex->getMessage());
+        }
+    }
 
-       public function vehicleBooking(Request $request){
-       
+
+    public function delete($id)
+    {
+        try {
+            $response = $this->vehicleService->delete($id);
+            if ($response) {
+                return response()->json([
+                    "isExecute" => true,
+                    "data" => $response,
+                    "message" => "Vehicle Deleted"
+                ], 200);
+            } else {
+                return response()->json([
+                    "isExecute" => true,
+                    "message" => "Data Can not be deleted"
+                ], 200);
+            }
+        } catch (Exception $ex) {
+            Log::error($ex->getMessage());
+        }
+    }
+
+    public function dropdown()
+    {
+        $response = $this->vehicleService->dropdown();
+        return response()->json([
+            "data" => $response,
+            "message" => "success"
+        ], 200);
+    }
+
+    public function vehicleBooking(Request $request)
+    {
+
         $response = $this->vehicleService->vehicleBooking($request->all());
         $trackingResponse  = $this->vehicleService->vehicleTripTrackings($request->all());
 
-        if($response==true && $trackingResponse==true){
+        if ($response['status'] == true && $trackingResponse == true) {
             return response()->json([
                 'isExecute' => true,
                 'data' => $response,
-                'message' => 'New Vehicle Created',
-            ],200);
-           }
-       }
-
-
-
+                'message' => $response['message'],
+            ], 200);
+        } else {
+            return response()->json([
+                'isExecute' => false,
+                'data' => $response,
+                'message' => $response['message'],
+            ], 200);
+        }
+    }
 }
--- app/Repository/Services/Vehicles/VehicleService.php
+++ app/Repository/Services/Vehicles/VehicleService.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Repository\Services\Vehicles;
+
 use App\Repository\Interfaces\CommonInterface;
 use App\route;
 use Exception;
 use Illuminate\Support\Facades\Log;
 use DB;
 
-class VehicleService implements CommonInterface{
+class VehicleService implements CommonInterface
+{
 
     protected $contact;
 
@@ -16,29 +18,30 @@ class VehicleService implements CommonInterface{
      *
      * @return bool
      */
-    public function index($page,$search)
+    public function index($page, $search)
     {
 
-        try{
+        try {
             $perPage = 10;
-           $vehicles = DB::table('vehicles') 
-                    ->join('routes', 'vehicles.route_id', '=', 'routes.id')
-                    ->where('vehicle_type', 'like', '%' . $search . '%')
-                    ->orWhere('vehicle_name', 'like', '%' . $search . '%')
-                    ->orWhere('total_seats', 'like', '%' . $search . '%')
-                    ->paginate($perPage, ['vehicles.id','vehicle_type','vehicle_name','total_seats','routes.route_name'], 'page', $page);
-           return $vehicles;
-        }catch(Exception $ex){
+            $vehicles = DB::table('vehicles')
+                ->join('routes', 'vehicles.route_id', '=', 'routes.id')
+                ->where('vehicle_type', 'like', '%' . $search . '%')
+                ->orWhere('vehicle_name', 'like', '%' . $search . '%')
+                ->orWhere('total_seats', 'like', '%' . $search . '%')
+                ->paginate($perPage, ['vehicles.id', 'vehicle_type', 'vehicle_name', 'total_seats', 'routes.route_name'], 'page', $page);
+            return $vehicles;
+        } catch (Exception $ex) {
             Log::alert($ex->getMessage());
         }
     }
 
-    public function store($request){
-        try{
+    public function store($request)
+    {
+        try {
             $vehicleInsert = DB::table('vehicles')->insertGetId($request);
             $seatLayout = $this->generateSeats($request['total_seats']);
             Log::info($seatLayout);
-            foreach($seatLayout as $seat){
+            foreach ($seatLayout as $seat) {
                 $seatData = [
                     'vehicle_id' => $vehicleInsert,
                     'seat_number' => $seat,
@@ -50,72 +53,87 @@ class VehicleService implements CommonInterface{
             }
 
 
-            if($vehicleInsert){
+            if ($vehicleInsert) {
                 return true;
             }
             return false;
-        }catch(Exception $ex){
-            Log::alert("Insert error: ".$ex->getMessage());
+        } catch (Exception $ex) {
+            Log::alert("Insert error: " . $ex->getMessage());
         }
     }
 
 
-    public function findById($id){
-        try{
-            $findVehicleById = DB::table('vehicles')->where('id',$id)->first();
+    public function findById($id)
+    {
+        try {
+            $findVehicleById = DB::table('vehicles')->where('id', $id)->first();
             return $findVehicleById;
-        }catch(Exception $ex){
-            Log::alert("Find By Id Error".$ex->getMessage());
+        } catch (Exception $ex) {
+            Log::alert("Find By Id Error" . $ex->getMessage());
         }
     }
 
-    public function delete($id){
-        try{
-            $response =DB::table('vehicles')->where('id',$id)->delete();
+    public function delete($id)
+    {
+        try {
+            $response = DB::table('vehicles')->where('id', $id)->delete();
             return $response;
-        }catch(Exception $ex){
-            Log::alert("Delete Error".$ex->getMessage());
+        } catch (Exception $ex) {
+            Log::alert("Delete Error" . $ex->getMessage());
         }
     }
 
-    public function dropdown(){
-        try{
-           $vehicles = DB::table('vehicles')->select('id','vehicle_name')->get();
-           return $vehicles;
-        }catch(Exception $ex){
+    public function dropdown()
+    {
+        try {
+            $vehicles = DB::table('vehicles')->select('id', 'vehicle_name')->get();
+            return $vehicles;
+        } catch (Exception $ex) {
             Log::alert($ex->getMessage());
         }
     }
 
 
-    public function vehicleBooking($data){
-        try{
+    public function vehicleBooking($data)
+    {
+        try {
             $tripId = $data['trip_id'];
             $vehicleId = $data['vehicle_id'];
-            $seats = DB::table('seats')->where('vehicle_id',$vehicleId)->get();
+            $tripInformation = DB::table('trips')->where('id', $tripId)->first();
+            $alreadyBooked = DB::table('vehicle_trip_trackings')->where('vehicle_id', $vehicleId)
+                ->where(function ($query) use ($tripInformation) {
+                    $query->whereBetween('travel_start_date', [$tripInformation->departure_time, $tripInformation->arrival_time])
+                        ->orWhereBetween('travel_end_date', [$tripInformation->departure_time, $tripInformation->arrival_time]);
+                })->first();
+
+            if ($alreadyBooked) {
+                return ['status' => true, 'message' => 'This Vehicle Is Already Booked For Given Trip Dates'];
+            }
+            $seats = DB::table('seats')->where('vehicle_id', $vehicleId)->get();
             $insertedData = [];
             foreach ($seats as $seat) {
                 $insertedData[] = [
                     'trip_id' => $tripId,
-                    'seat_id' => $seat->id 
+                    'seat_id' => $seat->id
                 ];
             }
             $vehicleInsert = DB::table('seat_availablities')->insert($insertedData);
-            if($vehicleInsert){
-                return true;
+            if ($vehicleInsert) {
+                return ['status' => true, 'message' => 'Vehicle booked successfully.'];
             }
-            return false;
-        }catch(Exception $ex){
+            return ['status' => false, 'message' => 'Vehicle booking failed.'];
+        } catch (Exception $ex) {
             Log::alert($ex->getMessage());
         }
     }
 
 
-    public function vehicleTripTrackings($data){
-        try{
+    public function vehicleTripTrackings($data)
+    {
+        try {
             $tripId = $data['trip_id'];
             $vehicleId = $data['vehicle_id'];
-            $tripInformation  = DB::table('trips')->where('id',$tripId)->first();
+            $tripInformation  = DB::table('trips')->where('id', $tripId)->first();
             $trackingInformationToInsert = [
                 'trip_id' => $tripId,
                 'vehicle_id' => $vehicleId,
@@ -127,30 +145,28 @@ class VehicleService implements CommonInterface{
             $trackingInformationStore = DB::table('vehicle_trip_trackings')->insert($trackingInformationToInsert);
             Log::info($trackingInformationToInsert);
 
-            if($trackingInformationStore){
+            if ($trackingInformationStore) {
                 return true;
             }
             return false;
-        }catch(Exception $ex){
+        } catch (Exception $ex) {
             Log::alert($ex->getMessage());
         }
     }
 
 
-    function generateSeats($number_of_seats) {
-        $rows = ['A', 'B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ'];
+    function generateSeats($number_of_seats)
+    {
+        $rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ'];
         $seatsPerRow = 4;
         $seats = [];
-    
+
         for ($i = 0; $i < $number_of_seats; $i++) {
-            $row = $rows[intdiv($i, $seatsPerRow)]; 
+            $row = $rows[intdiv($i, $seatsPerRow)];
             $seat = ($i % $seatsPerRow) + 1;
             $seats[] = $row . $seat;
         }
-     
+
         return $seats;
     }
-    
-
-
 }
