679326a1710ec9158ae022f94fc9bbf39e565e35
Anik <sahaanik1045@gmail.com>
2026-02-21 00:50:33 +0600

    status added

--- app/Repository/Services/Ticket/TicketService.php
+++ app/Repository/Services/Ticket/TicketService.php
@@ -67,6 +67,7 @@ class TicketService
                                             'tickets.description',
                                             'tickets.generate_by',
                                             'tickets.status',
+                                            'tickets.resolved_status',
                                             'tickets.remarks',
                                             'tickets.attachment',
                                             'tickets.created_at',
